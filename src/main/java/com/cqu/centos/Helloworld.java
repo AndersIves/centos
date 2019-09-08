f381deda2c49004d27faf92d2bc67b795c48cc69
AndersIves <13895637677@126.com>
2019-09-08 11:47:51 +0800

    Initial commit

---
+++ src/main/java/com/cqu/centos/Helloworld.java
@@ -0,0 +1,14 @@
+package com.cqu.centos;
+
+/**
+ * @author AndersIves
+ * @date 2019/9/8 上午 11:42
+ */
+public class Helloworld {
+    public static void main(String[] args) throws Exception{
+        while (true) {
+            System.out.println("HelloWorld");
+            Thread.sleep(1000*3);
+        }
+    }
+}
